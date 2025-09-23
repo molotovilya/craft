054dab3f26de24315552284b27fd2cdd78dc3d55
Ilya Molotov <Molotovilyasrg@gmail.com>
2025-09-23 13:20:12 +0700

    Update: 23.09.2025 13:20:12,33

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,6 +6,13 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
+  footer: [
+    Component.Footer({
+      links: {
+        "": "",
+      },
+    }),
+  ],
 }
 
 // components for pages that display a single page (e.g. a single note)

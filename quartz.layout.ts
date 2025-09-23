44056e5f6d8c32f679b194dc146d52e46bcb7528
Ilya Molotov <Molotovilyasrg@gmail.com>
2025-09-23 13:46:12 +0700

    Update: 23.09.2025 13:46:12,73

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,14 +5,13 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],footer: [
-    Component.Footer({
-      links: {
-        GitHub: "https://github.com/jackyzha0/quartz",
-        "Discord Community": "https://discord.gg/cRFFHYye7t",
-      },
-    }),
-  ],
+  afterBody: [],
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
+    },
+  }),
 }
 
 // components for pages that display a single page (e.g. a single note)
@@ -48,7 +47,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages (e.g. tags or folders)
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [

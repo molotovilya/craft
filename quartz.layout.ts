c2d0747e014fc0b07d403845613937744d33cd5b
Ilya Molotov <Molotovilyasrg@gmail.com>
2025-09-23 12:51:26 +0700

    Update: 23.09.2025 12:51:26,43

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -11,6 +11,7 @@ export const sharedPageComponents: SharedLayout = {
       links: {}
     }),
   ],
+}
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
@@ -45,7 +46,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
+// components for pages that display lists of pages (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [

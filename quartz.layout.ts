a26be763b64180a29e093550f9c173f573c246af
Ilya Molotov <Molotovilyasrg@gmail.com>
2025-09-23 12:53:26 +0700

    Update: 23.09.2025 12:53:26,08

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,9 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   afterBody: [],
   footer: [
-    Component.Footer({
-      links: {}
-    }),
+    Component.Footer(),
   ],
 }
 
